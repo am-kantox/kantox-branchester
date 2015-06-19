dc6d770c39ad521fcd2f2d83c9b561e83635cdfd
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2015-06-19 16:49:10 +0200

    Time to ckeck.

--- kantox-branchester.gemspec
+++ kantox-branchester.gemspec
@@ -25,11 +25,11 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(/^exe\//) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  spec.add_dependency 'ruby-git', '~> 0.2'
+  spec.add_dependency 'git', '~> 1.2'
 
   spec.add_development_dependency 'bundler', "~> 1.10"
   spec.add_development_dependency 'rake', "~> 10.0"
   spec.add_development_dependency 'rspec', '~> 3.2'
 
-  spec.add_development_dependency 'pry', '~> 0.10'  
+  spec.add_development_dependency 'pry', '~> 0.10'
 end
--- lib/kantox/branchester.rb
+++ lib/kantox/branchester.rb
@@ -1,8 +1,48 @@
-require "kantox/branchester/version"
+require 'kantox/branchester/version'
+require 'logger'
+require 'git'
 
 module Kantox
   module Branchester
     THREADS = 4
-    # Your code goes here...
+
+    class Error < ::StandardError ; end
+
+    class Yo
+      def initialize dir = nil
+        @githome = dir || Dir.pwd
+        @logger = ::Logger.new STDOUT
+        @logger.level = ::Logger::INFO
+        @mx = Mutex.new
+        @result = []
+      end
+
+      def check threads = THREADS
+        g = Git.open(@githome, :log => @logger)
+        remotes = g.branches.remote.each_slice threads
+        remotes.map do |branches|
+          Thread.new do
+            result = check_branches @githome, branches.dup
+            @mx.synchronize { @result << result }
+          end
+        end.reduce &:join
+        @result
+      end
+
+      def check_branches local, branches
+        branches.inject({}) do |memo, b|
+          Dir.mktmpdir do |temp|
+            Dir.chdir temp
+            g = Git.clone local
+            memo[b] = g.merge b
+          end
+          memo
+        end
+      end
+    end
+
+    def self.yo threads = THREADS
+      Yo.new.check threads
+    end
   end
 end
