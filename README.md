917767e54f06c25147b21b880c43c10a8d8452b5
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2015-06-24 13:57:05 +0200

    Viable product.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Kantox::Branchester
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/kantox/branchester`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+Include this gem into your gemfile and stay notified about all the possible
+merge conflicts against all the currently active branches.
 
 ## Installation
 
@@ -22,7 +21,74 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+Very first run will be long, since the information about all the active
+branches is to be gathered. The subsequent run should not take long since
+the results are cached.
+
+All logs are stored in `branchester` directory. The configuration is located
+in `config/branchester.yml`. The file will appear after first run.
+
+Typical use case would be to include `bundle exec branchester` into local
+`post-commit` hook, or to run it manually. Please note, that once blacklisted,
+the branch would not be checked against until explicitly removed from
+`config/branchester.yml`.
+
+Typical output:
+
+#### For last 30 days / 1000 commits
+
+```bash
+Will try to merge 28 branches:
+============================
+****************************
+============================
+
+Some branches failed to merge automatically into feature/check-branchester
+feature/CO-32_CM_sha_dir_eme ==>
+  Recorded preimage for 'Gemfile.lock'
+	Recorded preimage for 'db/schema.rb'
+	CONFLICT (content): Merge conflict in db/schema.rb
+	CONFLICT (content): Merge conflict in Gemfile.lock
+	CONFLICT (content): Merge conflict in Gemfile
+	Automatic merge failed; fix conflicts and then commit the result.
+
+hotfix/PT-2246-demo-platform-phone-quotes ==>
+  CONFLICT (content): Merge conflict in app/mutations/liquidity_provider_router.rb
+	Automatic merge failed; fix conflicts and then commit the result.
+
+vemv-letter-opener-indication ==>
+  CONFLICT (content): Merge conflict in config/environments/development.rb
+	Automatic merge failed; fix conflicts and then commit the result.
+
+```
+
+#### For last 7 days / 1000 commits
+
+```bash
+Will try to merge 7 branches:
+=======
+*******
+=======
+
+Everything merged successfully into feature/check-branchester
+```
+
+## Configuration
+
+```yaml
+--- !ruby/hash:Hashie::Mash
+config: !ruby/hash:Hashie::Mash
+  age: 7
+  retro: 1000
+```
+
+The above means only branches, not older than 7 days and only last 1000 commits
+will be taken into account.
+
+## To Do
+
+  * «urgent» update a list of active branches once on a startup;
+  * accept a `--full` parameter in the command line, forcing to ignore blacklist.
 
 ## Development
 
@@ -38,4 +104,3 @@ Bug reports and pull requests are welcome on GitHub at https://github.com/[USERN
 ## License
 
 The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
-
--- lib/kantox/branchester.rb
+++ lib/kantox/branchester.rb
@@ -19,7 +19,12 @@ end
 module Kantox
   module Branchester
     THREADS = 4
+    DEFAULT_LOG_COUNT = 1_000
     DEFAULT_BRANCH_AGE = 7
+    HEAD = 'head'
+    ORIGIN = 'origin'
+    REMOTES = 'remotes'
+    TAGS = 'tags'
     PREFIX = 'bch-' # prefix for new origin
 
     class Error < ::StandardError ; end
@@ -27,83 +32,138 @@ module Kantox
     class Yo
       include ::Kungfuig
 
+      attr_reader :git_config, :branches, :current
+
       def initialize dir = nil
         @githome = dir || Dir.pwd
-        @logger = ::Logger.new STDOUT
-        @logger.level = ::Logger::INFO
+        (@logger = ::Logger.new STDOUT).level = ::Logger::WARN
         @mx = Mutex.new
-        @result = []
-        @cfg_file, @log_file = prepare_files
+        @now = Time.now
 
+        @logger.info "Preparing config and log..."
+        @cfg_file, @log_file = prepare_files
         config @cfg_file
+
+        @git_config = Hashie::Mash.new(
+          retro: options.config!.retro || DEFAULT_LOG_COUNT,
+          age: options.config!.age || DEFAULT_BRANCH_AGE,
+          keep: options.config!.keep || []
+        )
+
+        @data = []
+        @current = nil
+        @remote = nil
+        @branches = nil
       end
 
-      def check threads = THREADS
+      # thread unsafe
+      def init
         g = Git.open(@githome, :log => @logger)
-        remotes = g.branches.remote
-        if options.config!.skip.is_a?(Array) && !options.config!.skip.empty?
-          rejected = remotes.select { |b| options.config!.skip.include? b.name }
-          rejected_as_string = rejected.join("\n\t")
-          puts "Will not proceed following branches:\n\t#{rejected_as_string}"
-          remotes -= rejected
-          puts "Total branches to proceed: #{remotes.size}:\n"
+        @current = g.lib.branch_current
+        @remote = g.remote
+        remotes = g.branches.remote.map(&:name).map { |b| [b, nil] }.to_h # full list
+
+        # FIXME clone ⇒ fetch ⇒ deal ??? Could it be a case that we don’t have all branches here?
+        # remote_prefix = "#{PREFIX}-remote"
+        # g.add_remote remote_prefix, remote, fetch: true
+
+        # FIXME at least g.fetch ??
+
+        branches = Git::Log.new(g, @git_config.retro).inject(remotes) do |memo, gl|
+          b = branch?(desc = g.describe(gl.sha, contains: true, all: true))
+          memo[b.name] ||= {
+            data: b,
+            desc: desc,
+            last: gl.sha,
+            date: gl.date,
+            committer: gl.committer,
+            info: g.lib.show(gl.sha),
+            age: ((@now - gl.date) / (60*60*24)).floor + 1
+          }
+          # FIXME we probably can break here, if commits are sorted by date. Are they?
+          break memo unless memo.values.any? &:nil?
+          memo
+        end.reject do |name, info|
+          (!@git_config.keep.include?(name) &&  # not defined to keep explicitly
+          (
+            info.nil? ||                        # HEAD -> origin/master
+            info[:age].nil? ||                  # older than last @git_config.retro commits
+            info[:age] > @git_config.age        # older than max age as by @git_config.age
+          )).tap do |tf|
+            @logger.warn (tf ? "- REJECTED #{name} branch (#{info && info[:age]})" : "* SELECTED #{name} branch")
+          end
+        end
+      end
+      private :init
+
+      def check threads = THREADS
+        unless @branches
+          @logger.warn "Gathering all repo info [go have a ristretto]..."
+          @branches = init
         end
 
-        unless remotes.size.zero?
-          slices = remotes.size.divmod threads
-          remotes.each_slice(slices.first + (slices.last.zero? ? 0 : 1)).map do |branches|
+        unless @branches.size.zero?
+          slices = @branches.size.divmod threads
+          puts "\n\e[01mWill try to merge \e[01;38;05;68m#{@branches.size}\e[0m\e[01m branches:\e[0m"
+          puts "=" * @branches.size
+          @branches.each_slice(slices.first + (slices.last.zero? ? 0 : 1)).map do |branches|
             Thread.new do
-              result = check_branches @githome,
-                                      g.config['remote.origin.url'],
-                                      g.lib.branch_current,
-                                      branches #.map { |b| "#{b.remote.name}/#{b.name}" } # (&:full) #
-              @mx.synchronize { @result << result }
+              result = check_bulk @githome,
+                                  @current,
+                                  branches
+              @mx.synchronize { @data << result }
             end
           end.each &:join
-          @result = @result.reduce(&:merge)
+          puts
+          puts "=" * @branches.size
+          @data = @data.reduce(&:merge)
 
-          File.write @log_file, @result.to_yaml
+          File.write @log_file, @data.to_yaml
           File.write @cfg_file, options.to_yaml
         else
           @logger.warn "Everything is up-to-date. Too lazy check settings?"
         end
 
-        [ @result, @options ]
+        [@data, self]
       end
 
-      def check_branches local, remote, branch, branches
-        branches.inject({}) do |memo, b|
+      def check_bulk local, current, branches
+        # `(name, b)` below are:
+        #     "superclass-for-kantox-errors"=> {
+        #         :data=>{"name"=>"superclass-for-kantox-errors", "full"=>"remotes/origin/superclass-for-kantox-errors", "revision"=>nil, "remote"=>true, "tag"=>false},
+        #         :desc=>"remotes/origin/superclass-for-kantox-errors",
+        #         :last=>"c11af7c71cad3e090407d40bfdcffce00a8e2adc",
+        #         :date=>2015-06-10 08:51:59 +0200,
+        #         :committer=>#<Git::Author:0x0000000385fa68 @date=2015-06-10 08:51:59 +0200, @email="aleksei.matiushkin@gmail.com", @name="Aleksei Matiushkin">,
+        #         :info=> "LONG_STRING_GARBAGE"
+        #         :age=>14.087258708528935}
+        #     }
+        branches.inject({}) do |memo, (name, b)|
           temp = Dir.mktmpdir
           begin
-            g = Git.clone local, 'clone', path: temp
-            g.add_remote "#{PREFIX}-#{b.remote.name}", remote, fetch: true
-            case branch_state b.name, g.log.to_a.first
-            when :obsolete
-              @logger.info "[#{Thread.current}] ⇒ skipping branch: #{b.name} ⇒ :obsolete (age: #{options.branches[b.name].age} days)"
-              print '-' if @logger.level > Logger::INFO
-              (options.config!.skip ||= []) << b.name
-              next memo
-            when :boi
-              @logger.info "[#{Thread.current}] ⇒ processing branch: #{b.name}"
-              print '*' if @logger.level > Logger::INFO
-            else raise 'Lame programmer error'
-            end
-            memo[b.name] =  begin
-                              case msg = g.branches[branch].merge(g.branches["#{PREFIX}-#{b.remote.name}/#{b.name}"])
-                              when /'origin\/master'/ then { success: { branch: b, onmaster: msg } }
-                              when /up-to-date/ then { success: { branch: b, uptodate: msg } }
-                              else { success: { branch: b, merged: msg } }
-                              end
-                            rescue Git::GitExecuteError => e
-                              { branch: b, error: e.message.split($/)[1..-1] }
-                            end
+            remote_prefix = "#{PREFIX}-#{@remote.name}"
+            g = Git.clone local, 'branchester', path: temp
+            g.add_remote remote_prefix, @remote.url, fetch: true # FIXME FUCK HOW NOT TO FETCH EVERYTHING?? merge ⇒ revert/stash/rebase --abort ???
+
+            memo[name] =  { branch: b }.merge begin
+                                                case msg = g.branches[@current].merge(g.branches["#{remote_prefix}/#{name}"])
+                                                when /'origin\/master'/ then { status: :success, reason: :onmaster, message: msg }
+                                                when /up-to-date/ then { status: :success, reason: :uptodate, message: msg }
+                                                else { status: :success, reason: :merged, message: msg }
+                                                end
+                                              rescue Git::GitExecuteError => e
+                                                { status: :error, message: e.message.split($/)[1..-1] }
+                                              end
+            print '*'
           ensure
             FileUtils.rm_rf temp
           end
           memo
         end
       end
+      private :check_bulk
 
+      # Prepares config and log directories / files.
       def prepare_files
         cfgdir = File.join @githome, 'config'
         Dir.mkdir(cfgdir) unless File.exist?(cfgdir)
@@ -121,30 +181,38 @@ module Kantox
         [cfg_file, log_file]
       end
 
-      def branch_state branch, logstamp
-        options.branches ||= Hashie::Mash.new
-        options.branches[branch] = Hashie::Mash.new(
-          checked: Time.now,
-          modified: logstamp.committer.date,
-          blame: logstamp.committer.email,
-          message: logstamp.message,
-          sha: logstamp.sha
-        );
-        options.branches[branch].merge!(age: (options.branches[branch].checked - options.branches[branch].modified).round / (60*60*24) )
-
-        if options.branches[branch].age > (options.config!.age || DEFAULT_BRANCH_AGE)
-          :obsolete
-        else
-          :boi
-        end
+      # Returns extended branch naming
+      def branch? id, use_origin = ORIGIN
+        id, revision = id.split('~', 2)
+        type, origin, name =  case id
+                              when /\A#{REMOTES}\//
+                                id.split('/', 3)
+                              when /\A#{TAGS}\//
+                                [nil, id.split('/', 2)]
+                              else [false, '', id]
+                              end
+        Hashie::Mash.new(
+          name: name || HEAD,
+          full: "#{REMOTES}/#{use_origin}/#{name}",
+          revision: revision,
+          remote: type.is_a?(String),
+          tag: type.nil?
+        )
       end
-      private :prepare_files, :branch_state
+
+      private :prepare_files, :branch?
     end
 
     def self.yo threads = THREADS
-      Yo.new.check(threads).tap do |res, opt|
-        puts "\n\n\e[01mSome branches failed to merge automatically:\e[0m\n"
-        puts res.select { |_, v| v[:error] }.map { |(k, v)| [k, v[:error].join("\n\t")].join("\n\t") }.join("\n\n").gsub(/(CONFLICT)(.*?)(\S+)$/, "\e[01;38;05;196m\\1\e[0m\\2\e[01;38;05;68m\\3\e[0m")
+      Yo.new.check(threads).tap do |res, yo|
+        fails = res.select { |_, v| v[:status] == :error }
+        status = fails.empty? ? "Everything merged successfully" : "Some branches failed to merge automatically"
+        puts "\n\e[01m#{status} into #{yo.current}\e[0m\n"
+        fails.each do |k, v|
+          puts "#{k} ==>\n\t"
+          puts v[:message].strip.join("\n\t").gsub(/(CONFLICT)(.*?)(\S+)$/, "\e[01;38;05;196m\\1\e[0m\\2\e[01;38;05;68m\\3\e[0m")
+          puts
+        end
       end
     end
   end
